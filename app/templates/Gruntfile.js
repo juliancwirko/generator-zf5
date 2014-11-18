cb098c7fc488383fcdb4efc3ff45f938674abacd
Ruggero <ruggero.motta@gmail.com>
2014-11-18 16:20:03 +0100

    autoprefixer task added

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -4,85 +4,48 @@ module.exports = function(grunt) {
 
     require('load-grunt-tasks')(grunt);
 
-
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		app: 'app',
 		dist: 'dist',
 
-		sass: {<% if (!compass) { %>
+		sass: {
 			options: {
-				includePaths: ['<%%= app %>/bower_components/foundation/scss']
+				includePaths: ['<%= app %>/bower_components/foundation/scss'],
+				sourceMap: true
 			},
 			dist: {
 				options: {
 					outputStyle: 'extended'
 				},
 				files: {
-					'<%%= app %>/css/app.css': '<%%= app %>/scss/app.scss'
+					'<%= app %>/css/app.css': '<%= app %>/scss/app.scss'
 				}
-			}<% } else { %>
-			dist: {
-				options: {
-					style: 'expanded', // expanded or nested or compact or compressed
-					loadPath: '<%%= app %>/bower_components/foundation/scss',
-					compass: true,
-					quiet: true
-				},
-				files: {
-					'<%%= app %>/css/app.css': '<%%= app %>/scss/app.scss'
-				}
-			}<% } %>
-		},
-
-		<% if (jade) { %>
-		jade: {
-			compile: {
-				options: {
-					pretty: true,
-					data: {
-						debug: false
-					}
-				},
-				files: [{
-					expand: true,
-					cwd: '<%%= app %>/',
-					src: ['**/*.jade', '!**/header.jade', '!**/footer.jade'],
-					ext: '.html',
-					dest: '<%%= app %>/'
-				}]
 			}
-		},<% } %>
-
-		jshint: {
-			options: {
-				jshintrc: '.jshintrc'
-			},
-			all: [
-				'Gruntfile.js',
-				'<%%= app %>/js/**/*.js'
-			]
 		},
+
+		autoprefixer: {
+		      options: {
+		        browsers: ['last 2 version', 'ie 8', 'ie 9']
+		      },
+		      all: {
+		        src: ['<%= app %>/css/app.css']
+		      }
+		    },
 
 		clean: {
 			dist: {
-				src: ['<%%= dist %>/*']
+				src: ['<%= dist %>/*']
 			},
 		},
 		copy: {
 			dist: {
 				files: [{
 					expand: true,
-					cwd:'<%%= app %>/',
+					cwd:'<%= app %>/',
 					src: ['fonts/**', '**/*.html', '!**/*.scss', '!bower_components/**'],
-					dest: '<%%= dist %>/'
-				}<% if (fontAwesome) { %> , {
-					expand: true,
-					flatten: true,
-					src: ['<%%= app %>/bower_components/font-awesome/fonts/**'],
-					dest: '<%%= dist %>/fonts/',
-					filter: 'isFile'
-				} <% } %>]
+					dest: '<%= dist %>/'
+				}]
 			},
 		},
 
@@ -90,9 +53,9 @@ module.exports = function(grunt) {
 			target: {
 				files: [{
 					expand: true,
-					cwd: '<%%= app %>/images/',
+					cwd: '<%= app %>/images/',
 					src: ['**/*.{jpg,gif,svg,jpeg,png}'],
-					dest: '<%%= dist %>/images/'
+					dest: '<%= dist %>/images/'
 				}]
 			}
 		},
@@ -105,17 +68,17 @@ module.exports = function(grunt) {
 		},
 
 		useminPrepare: {
-			html: ['<%%= app %>/index.html'],
+			html: ['<%= app %>/index.html'],
 			options: {
-				dest: '<%%= dist %>'
+				dest: '<%= dist %>'
 			}
 		},
 
 		usemin: {
-			html: ['<%%= dist %>/**/*.html', '!<%%= app %>/bower_components/**'],
-			css: ['<%%= dist %>/css/**/*.css'],
+			html: ['<%= dist %>/**/*.html', '!<%= app %>/bower_components/**'],
+			css: ['<%= dist %>/css/**/*.css'],
 			options: {
-				dirs: ['<%%= dist %>']
+				dirs: ['<%= dist %>']
 			}
 		},
 
@@ -125,15 +88,11 @@ module.exports = function(grunt) {
 				tasks: ['sass']
 			},
 			sass: {
-				files: '<%%= app %>/scss/**/*.scss',
-				tasks: ['sass']
-			},<% if (jade) { %>
-			jade: {
-				files: '<%%= app %>/**/*.jade',
-				tasks: ['jade']
-			},<% } %>
+				files: '<%= app %>/scss/**/*.scss',
+				tasks: ['sass', 'autoprefixer']
+			},
 			livereload: {
-				files: ['<%%= app %>/**/*.html', '!<%%= app %>/bower_components/**', '<%%= app %>/js/**/*.js', '<%%= app %>/css/**/*.css', '<%%= app %>/images/**/*.{jpg,gif,svg,jpeg,png}'],
+				files: ['<%= app %>/**/*.html', '!<%= app %>/bower_components/**', '<%= app %>/js/**/*.js', '<%= app %>/css/**/*.css', '<%= app %>/images/**/*.{jpg,gif,svg,jpeg,png}'],
 				options: {
 					livereload: true
 				}
@@ -144,7 +103,7 @@ module.exports = function(grunt) {
 			app: {
 				options: {
 					port: 9000,
-					base: '<%%= app %>/',
+					base: '<%= app %>/',
 					open: true,
 					livereload: true,
 					hostname: '127.0.0.1'
@@ -153,7 +112,7 @@ module.exports = function(grunt) {
 			dist: {
 				options: {
 					port: 9001,
-					base: '<%%= dist %>/',
+					base: '<%= dist %>/',
 					open: true,
 					keepalive: true,
 					livereload: false,
@@ -164,13 +123,11 @@ module.exports = function(grunt) {
 
 		wiredep: {
 			target: {
-				src: [<% if (jade) { %>
-					'<%%= app %>/**/*.jade'<% } else { %>
-					'<%%= app %>/**/*.html'<% } %>
+				src: [
+					'<%= app %>/**/*.html'
 				],
 				exclude: [
-					'modernizr',<% if (fontAwesome) { %>
-					'font-awesome',<% } %>
+					'modernizr',
 					'jquery-placeholder',
 					'jquery.cookie',
 					'foundation'
@@ -180,16 +137,15 @@ module.exports = function(grunt) {
 
 	});
 
-	<% if (jade) { %>grunt.registerTask('compile-jade', ['jade']);<% } %>
+	grunt.loadNpmTasks('grunt-autoprefixer');
+
 	grunt.registerTask('compile-sass', ['sass']);
 	grunt.registerTask('bower-install', ['wiredep']);
-	<% if (jade) { %>
-	grunt.registerTask('default', ['compile-jade', 'compile-sass', 'bower-install', 'connect:app', 'watch']);<% } else { %>
-	grunt.registerTask('default', ['compile-sass', 'bower-install', 'connect:app', 'watch']);<% } %>
+	
+	grunt.registerTask('default', ['compile-sass', 'autoprefixer', 'bower-install', 'connect:app', 'watch']);
 	grunt.registerTask('validate-js', ['jshint']);
 	grunt.registerTask('server-dist', ['connect:dist']);
-	<% if (jade) { %>
-	grunt.registerTask('publish', ['compile-jade', 'compile-sass', 'clean:dist', 'validate-js', 'useminPrepare', 'copy:dist', 'newer:imagemin', 'concat', 'cssmin', 'uglify', 'usemin']);<% } else { %>
-	grunt.registerTask('publish', ['compile-sass', 'clean:dist', 'validate-js', 'useminPrepare', 'copy:dist', 'newer:imagemin', 'concat', 'cssmin', 'uglify', 'usemin']);<% } %>
+	
+	grunt.registerTask('publish', ['compile-sass', 'clean:dist', 'validate-js', 'useminPrepare', 'copy:dist', 'newer:imagemin', 'concat', 'cssmin', 'uglify', 'usemin']);
 
 };
